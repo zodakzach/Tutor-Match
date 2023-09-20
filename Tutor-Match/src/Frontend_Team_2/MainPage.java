77cee79a5d1b2a2e4062332bfbed590fb6a188b5
Zachary Cervenka <zacharycervenka@Zacharys-MacBook-Pro.local>
2023-09-20 18:59:17 -0400

    Added Teams folder for frontend designs.

---
+++ Tutor-Match/src/Frontend_Team_2/MainPage.java
@@ -0,0 +1,45 @@
+package Frontend_Team_2;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JList;
+
+public class MainPage extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MainPage frame = new MainPage();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public MainPage() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		
+		JList list = new JList();
+		contentPane.add(list);
+	}
+
+}
