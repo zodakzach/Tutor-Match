9b8c3c8f19620894ed231e8cc5f72eff71f93a18
Zachary Cervenka <zacharycervenka@Zacharys-MacBook-Pro.local>
2023-09-22 21:28:32 -0400

    LoginPage design.

--- Tutor-Match/src/Frontend_Team_2/MainPage.java
+++ Tutor-Match/src/Frontend_Team_2/MainPage.java
@@ -14,10 +14,19 @@ import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
 import java.awt.ComponentOrientation;
 import javax.swing.LayoutStyle.ComponentPlacement;
+import java.awt.Color;
+import java.awt.BorderLayout;
+import javax.swing.JSplitPane;
+import javax.swing.BoxLayout;
+import java.awt.GridLayout;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
 
 public class MainPage extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textField;
+	private JPasswordField passwordField;
 
 	/**
 	 * Launch the application.
@@ -40,47 +49,93 @@ public class MainPage extends JFrame {
 	 */
 	public MainPage() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 876, 622);
 		contentPane = new JPanel();
+		contentPane.setBackground(new Color(0, 165, 255));
 		contentPane.setBorder(UIManager.getBorder("Button.border"));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
 		
-		JButton lblCreateAccount = new JButton("Create Account");
+		JPanel panel = new JPanel();
+		panel.setBounds(277, 69, 324, 444);
 		
-		JLabel lblTitle = new JLabel("Tutor-Match");
-		lblTitle.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
-		lblTitle.setFont(new Font("Lucida Grande", Font.BOLD, 60));
-		lblTitle.setHorizontalTextPosition(SwingConstants.CENTER);
-		lblTitle.setHorizontalAlignment(SwingConstants.CENTER);
+		JLabel lblNewLabel = new JLabel("Tutor-Match");
+		lblNewLabel.setFont(new Font("Lucida Grande", Font.PLAIN, 42));
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		
-		JButton lblLogin = new JButton("Login");
-		lblLogin.setHorizontalTextPosition(SwingConstants.CENTER);
-		GroupLayout gl_contentPane = new GroupLayout(contentPane);
-		gl_contentPane.setHorizontalGroup(
-			gl_contentPane.createParallelGroup(Alignment.TRAILING)
-				.addGroup(Alignment.LEADING, gl_contentPane.createSequentialGroup()
-					.addGap(81)
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
-						.addComponent(lblCreateAccount, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-						.addComponent(lblLogin, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 289, Short.MAX_VALUE))
-					.addGap(80))
-				.addGroup(gl_contentPane.createSequentialGroup()
-					.addGap(34)
-					.addComponent(lblTitle, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-					.addGap(25))
+		JLabel lblNewLabel_1 = new JLabel("Email:");
+		lblNewLabel_1.setHorizontalAlignment(SwingConstants.RIGHT);
+		
+		textField = new JTextField();
+		textField.setColumns(10);
+		
+		JLabel lblNewLabel_1_1 = new JLabel("Password:");
+		lblNewLabel_1_1.setHorizontalAlignment(SwingConstants.RIGHT);
+		
+		passwordField = new JPasswordField();
+		
+		JButton btnNewButton = new JButton("Login");
+		
+		JLabel lblNewLabel_2 = new JLabel("Don't have an account?");
+		
+		JButton btnNewButton_1 = new JButton("Sign Up");
+		btnNewButton_1.setBorderPainted(false);
+		btnNewButton_1.setHorizontalTextPosition(SwingConstants.LEFT);
+		btnNewButton_1.setHorizontalAlignment(SwingConstants.LEFT);
+		GroupLayout gl_panel = new GroupLayout(panel);
+		gl_panel.setHorizontalGroup(
+			gl_panel.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel.createSequentialGroup()
+					.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_panel.createSequentialGroup()
+							.addGap(6)
+							.addComponent(lblNewLabel, GroupLayout.DEFAULT_SIZE, 305, Short.MAX_VALUE))
+						.addGroup(gl_panel.createSequentialGroup()
+							.addGap(35)
+							.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
+								.addGroup(gl_panel.createSequentialGroup()
+									.addComponent(lblNewLabel_1)
+									.addPreferredGap(ComponentPlacement.RELATED, 238, Short.MAX_VALUE))
+								.addGroup(gl_panel.createSequentialGroup()
+									.addPreferredGap(ComponentPlacement.RELATED)
+									.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
+										.addGroup(gl_panel.createSequentialGroup()
+											.addComponent(lblNewLabel_1_1)
+											.addPreferredGap(ComponentPlacement.RELATED, 213, Short.MAX_VALUE))
+										.addGroup(gl_panel.createSequentialGroup()
+											.addComponent(lblNewLabel_2)
+											.addPreferredGap(ComponentPlacement.RELATED)
+											.addComponent(btnNewButton_1))
+										.addGroup(gl_panel.createParallelGroup(Alignment.TRAILING, false)
+											.addComponent(textField, Alignment.LEADING)
+											.addComponent(passwordField, Alignment.LEADING)
+											.addComponent(btnNewButton, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 254, Short.MAX_VALUE)))))
+							.addPreferredGap(ComponentPlacement.RELATED)))
+					.addGap(0))
 		);
-		gl_contentPane.setVerticalGroup(
-			gl_contentPane.createParallelGroup(Alignment.TRAILING)
-				.addGroup(Alignment.LEADING, gl_contentPane.createSequentialGroup()
-					.addGap(49)
-					.addComponent(lblTitle, GroupLayout.PREFERRED_SIZE, 72, GroupLayout.PREFERRED_SIZE)
-					.addGap(18)
-					.addComponent(lblLogin, GroupLayout.PREFERRED_SIZE, 29, GroupLayout.PREFERRED_SIZE)
+		gl_panel.setVerticalGroup(
+			gl_panel.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel.createSequentialGroup()
+					.addGap(14)
+					.addComponent(lblNewLabel, GroupLayout.PREFERRED_SIZE, 52, GroupLayout.PREFERRED_SIZE)
+					.addGap(32)
+					.addComponent(lblNewLabel_1)
 					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(lblCreateAccount, GroupLayout.PREFERRED_SIZE, 29, GroupLayout.PREFERRED_SIZE)
-					.addContainerGap(69, Short.MAX_VALUE))
+					.addComponent(textField, GroupLayout.PREFERRED_SIZE, 43, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(lblNewLabel_1_1)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(passwordField, GroupLayout.PREFERRED_SIZE, 45, GroupLayout.PREFERRED_SIZE)
+					.addGap(18)
+					.addComponent(btnNewButton, GroupLayout.PREFERRED_SIZE, 40, GroupLayout.PREFERRED_SIZE)
+					.addGap(66)
+					.addGroup(gl_panel.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblNewLabel_2)
+						.addComponent(btnNewButton_1))
+					.addContainerGap(55, Short.MAX_VALUE))
 		);
-		contentPane.setLayout(gl_contentPane);
+		panel.setLayout(gl_panel);
+		contentPane.add(panel);
 	}
 }
