8833159bc78788fc5e935d13c5d0f947cc056a15
Zachary Cervenka <zacharycervenka@Zacharys-MacBook-Pro.local>
2023-09-30 22:44:18 -0400

    Updated Frontend Design.

---
+++ Tutor-Match/src/Frontend_Team_2/SetTutorAvailability.java
@@ -0,0 +1,41 @@
+package Frontend_Team_2;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class SetTutorAvailability extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					SetTutorAvailability frame = new SetTutorAvailability();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public SetTutorAvailability() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+	}
+
+}
